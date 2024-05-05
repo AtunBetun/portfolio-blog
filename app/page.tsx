33591c27d0387c04b4740cd71a95ba121b18539c
Alberto De Saint Malo <beto7299@gmail.com>
2024-05-05 00:59:46 -0400

    adding content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 export default function Page() {
 	return (
 		<section>
-			<h1 className="font-medium text-2xl mb-8 tracking-tighter">hi i'm alberto 👋</h1>
+			<h1 className="font-medium text-2xl mb-8 tracking-tighter">
+				hi i'm alberto 👋
+			</h1>
+			<p>
+				i am a backend programmer working at blackstone
+			</p>
+			<br />
+			<p>
+				at night i am a bodybuilder training to compete in men's physique at the ocb, drug tested, bodybuilding federation.
+			</p>
+			<br />
+			<p>
+				my main passion is discovering new ways to challenge myself and dive deep into topics
+			</p>
+			<br />
+			<p>
+				creating this portofolio + blog is an example of this passion. i had an itch of not knowing how to develop and deploy frontend applications. so i decided to scratch it
+			</p>
 		</section>
 	);
 }
