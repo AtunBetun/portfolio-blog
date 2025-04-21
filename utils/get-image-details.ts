4f2d486d6f0aca8fe3209e14f0a0ea0347302ede
AtunBetun <beto7299@gmail.com>
2025-04-21 19:48:52 -0400

    remove unused

--- utils/get-image-details.ts
+++ utils/get-image-details.ts
@@ -3,20 +3,20 @@ import getBase64ImageUrl from "../lib/generate-blur-placeholder";
 import { ImageProps } from "./types";
 
 
-const GetImageDetils = async (reducedResults : any) => {
-  const blurImagePromises = reducedResults.map((image : any) => {
-    return getBase64ImageUrl(image.src);
-  });
-  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
+const GetImageDetils = async (reducedResults: any) => {
+    const blurImagePromises = reducedResults.map((image: any) => {
+        return getBase64ImageUrl(image.src);
+    });
+    const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
 
-  for (let i = 0; i < reducedResults.length; i++) {
-    reducedResults[i].blurDataURL = imagesWithBlurDataUrls[i];
-  }
-  return {
-    props: {
-      images: reducedResults,
-    },
-  };
+    for (let i = 0; i < reducedResults.length; i++) {
+        reducedResults[i].blurDataURL = imagesWithBlurDataUrls[i];
+    }
+    return {
+        props: {
+            images: reducedResults,
+        },
+    };
 
 }
 
